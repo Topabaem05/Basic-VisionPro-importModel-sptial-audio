715cc9115b76551f75623c1b940dbf435aaea2d4
Topabaem05 <choijjs83@gmail.com>
2024-01-24 00:11:33 +0900

    Initial Commit

---
+++ sptial-audio/sptial_audioApp.swift
@@ -0,0 +1,17 @@
+//
+//  sptial_audioApp.swift
+//  sptial-audio
+//
+//  Created by 구리뽕 on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct sptial_audioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
